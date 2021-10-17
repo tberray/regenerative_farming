e068dbf0a8d76d1e386053ade74d4468bc0f1afa
Spencer Ladd <srladd@uwm.edu>
2021-10-17 15:48:21 -0500

    sets up files for local environment

--- passportConfig.js
+++ passportConfig.js
@@ -3,34 +3,34 @@ const { pool } = require("./dbConfig");
 const bcrypt = require("bcrypt");
 
 function initialize (passport) {
-const authenticateUser = (email, password, done)=> {
+    const authenticateUser = (email, password, done)=> {
+        
+        pool.query(
+            `SELECT * FROM users WHERE email = $1`, [email], (err, results)=>{
+                if(err) {
+                    throw err;
+                }
+                console.log(results.rows);
     
-    pool.query(
-        `SELECT * FROM users WHERE email = $1`, [email], (err, results)=>{
-            if(err) {
-                throw err;
+                if(results.rows.length > 0) {
+                    const user = results.rows[0];
+                    bcrypt.compare(password, user.password, (err, isMatch)=>{
+                        if(err) {
+                            throw err
+                        }
+    
+                        if(isMatch) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false, {message: "Password is not correct" });
+                        }
+                    })
+                } else {
+                    return done(null, false, {message: "Email is not registered"});
+                }
             }
-            console.log(results.rows);
-
-            if(results.rows.length > 0) {
-                const user = results.rows[0];
-                bcrypt.compare(password, user.password, (err, isMatch)=>{
-                    if(err) {
-                        throw err
-                    }
-
-                    if(isMatch) {
-                        return done(null, user);
-                    } else {
-                        return done(null, false, {message: "Password is not correct" });
-                    }
-                })
-            } else {
-                return done(null, false, {message: "Email is not registered"});
-            }
-        }
-    )
-}
+        )
+    }
 
     passport.use(
         new LocalStrategy(
--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
-    secret: "secret", 
-    resave: false,
-    saveUninitialized: false
+	secret: "secret", 
+	resave: false,
+	saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -30,107 +30,105 @@ app.use(passport.session());
 app.use(flash());
 
 app.get('/', (req, res)=> {
-    res.render('index');
+	res.render('index');
 });
 
 app.get("/users/register", checkAuthenticated, (req, res)=> {
-    res.render("register");
+	res.render("register");
 });
 
 app.get("/users/login", checkAuthenticated, (req, res)=> {
-    res.render("login");
+	res.render("login");
 });
 
 app.get("/users/dashboard", checkNotAuthenticated, (req, res)=> {
-    res.render("dashboard", {user: req.user.name });
+	res.render("dashboard", {user: req.user.name });
 });
 
 app.get("/users/logout", (req, res)=>{
-    req.logOut();
-    req.flash("success_msg", "You have been logged out");
-    res.redirect("/users/login");
+	req.logOut();
+	req.flash("success_msg", "You have been logged out");
+	res.redirect("/users/login");
 });
 
 app.post("/users/register", async (req, res)=> {
-    let { name, email, password, password2 } = req.body;
-    console.log(name, email, password, password2);
+	let { name, email, password, password2 } = req.body;
+	console.log(name, email, password, password2);
 
-    let errors = [];
+	let errors = [];
 
-    if (!name || !email || !password || !password2) {
-        errors.push({message: "Please enter all fields" });
-    }
+	if (!name || !email || !password || !password2) {
+		errors.push({message: "Please enter all fields" });
+	}
 
-    if (password.length < 6) {
-        errors.push({message: "Password should be at least 6 characters" });
-    }
+	if (password.length < 6) {
+		errors.push({message: "Password should be at least 6 characters" });
+	}
 
-    if (password != password2) {
-        errors.push({message: "Passwords do not match" });
-    }
+	if (password != password2) {
+		errors.push({message: "Passwords do not match" });
+	}
 
-    if(errors.length > 0) {
-        res.render("register", { errors });
-    } else {
-        // form validation has passed
+	if(errors.length > 0) {
+		res.render("register", { errors });
+	} else {
+		// form validation has passed
 
-        let hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
-
-        pool.query(
-            `SELECT * FROM users
-            WHERE email = $1`, [email], (err, results)=>{
-                if (err) {
-                    throw err
-                }
-                console.log(results.rows);
-                if(results.rows.length > 0) {
-                    errors.push({message: "Email already registered"});
-                    res.render("register", { errors });
-                } else {
-                    pool.query(
-                        `INSERT INTO users (name, email, password)
-                        VALUES ($1, $2, $3)
-                        RETURNING id, password`, [name, email, hashedPassword], 
-                        (err, results)=>{
-                            if (err) {
-                                throw err
-                            }
-                            console.log(results.rows);
-                            req.flash("success_msg", "You are now registered. Please log in");
-                            res.redirect("/users/login");
-                        }
-                    );
-                }
-            }
-        );
-    }
+		pool.query(
+			`SELECT * FROM users
+			WHERE email = $1`, [email], (err, results)=>{
+				if (err) {
+					throw err
+				}
+				console.log(results.rows);
+				if(results.rows.length > 0) {
+					errors.push({message: "Email already registered"});
+					res.render("register", { errors });
+				} else {
+					let hashedPassword = await bcrypt.hash(password, 10);
+					pool.query(
+						`INSERT INTO users (name, email, password)
+						VALUES ($1, $2, $3)
+						RETURNING id, password`, [name, email, hashedPassword], 
+						(err, results)=>{
+							if (err) {
+								throw err
+							}
+							console.log(results.rows);
+							req.flash("success_msg", "You are now registered. Please log in");
+							res.redirect("/users/login");
+						}
+					);
+				}
+			}
+		);
+	}
 });
 
 app.post(
-    "/users/login", 
-    passport.authenticate("local", {
-        successRedirect: "/users/dashboard", 
-        failureRedirect: "/users/login", 
-        failureFlash: true
-    })
+	"/users/login", 
+	passport.authenticate("local", {
+		successRedirect: "/users/dashboard", 
+		failureRedirect: "/users/login", 
+		failureFlash: true
+	})
 );
 
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect("/users/dashboard");
-    }
-    next();
+	if (req.isAuthenticated()) {
+		return res.redirect("/users/dashboard");
+	}
+	next();
 }
 
 function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()){
-        return next()
-    }
+	if (req.isAuthenticated()){
+		return next()
+	}
 
-    res.redirect("/users/login");
+	res.redirect("/users/login");
 }
 
 app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 })
