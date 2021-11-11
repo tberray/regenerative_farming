7f0fd98f564bfaa800fdc73a095c30c715cbf0ef
Tyler Berray <tberray@hotmail.com>
2021-11-10 23:24:35 -0600

    sidenav in /users/test thats close to what we want

--- routes/users.js
+++ routes/users.js
@@ -29,6 +29,10 @@ module.exports = (params) => {
 	router.get("/about", (req, res)=> {
 		res.render("about");
 	});
+
+	router.get("/test", (req, res)=> {
+		res.render("test");
+	});
 	
 	router.get("/logout", (req, res)=>{
 		req.logOut();
